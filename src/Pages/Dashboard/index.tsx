405991a9287d18b53bc2575df829ebe374ac87ed
Kaung Min Khant <kmkhant1500@gmail.com>
2025-06-12 21:17:46 +0630

    Feat : fix dashboard

--- src/Pages/Dashboard/index.tsx
+++ src/Pages/Dashboard/index.tsx
@@ -1,14 +1,14 @@
-import UnderConstructionIcon from "../../Components/Icons/AnimatedIcons/Dashboard/UnderConstructionIcon"
+// import UnderConstructionIcon from "../../Components/Icons/AnimatedIcons/Dashboard/UnderConstructionIcon"
 
 const Dashboard = () => {
     return (
         <div className="flex h-screen items-center justify-center">
             <div className="w-[50rem] relative">
                 <h4 className="absolute top-20 left-1/2 -translate-x-1/2">
-                    This Page is under construction!
+                    Create Update Delete and have fun!
                 </h4>
 
-                <UnderConstructionIcon />
+                {/* <UnderConstructionIcon /> */}
 
             </div>
         </div>
